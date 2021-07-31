4a3f322074ab745eba4c11d6ee66cba435f4f135
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-07-31 16:07:09 +0300

    dynamically sized circle widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(){
-
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
@@ -28,23 +28,37 @@ class MyHomePage extends StatefulWidget{
 class SnakeWidget extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Center(
+          child: CustomPaint(
+            painter: CirclePainter(100, newCircleSize: 200),
+          ),
+        ),
+      ),
+    );
   }
 }
 
 class CirclePainter extends CustomPainter{
+  int deviation=0;
+  double circleSize=25.0;
+  CirclePainter(int offsetDeviation,{double newCircleSize=25}){
+    deviation = offsetDeviation;
+    circleSize = newCircleSize;
+  }
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
       ..color = Colors.teal
       ..strokeWidth = 15;
 
-    Offset center = Offset(size.width / 2, size.height / 2);
+    Offset center = Offset(size.width / 2 + deviation, size.height / 2 + deviation);
 
-    canvas.drawCircle(center, 25, paint);
+    canvas.drawCircle(center, circleSize, paint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
