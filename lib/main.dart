33e949b53ae498befa47d01759ba0d080c9fd4a5
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-01 13:55:50 +0300

    added a line to represent the snake and changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Snake',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColorBrightness: Brightness.light,
-      ),
+      theme: ThemeData.dark(),
       home: MyHomePage(),
     );
   }
@@ -35,22 +32,47 @@ class SnakeWidget extends State<MyHomePage>{
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Center(
-          child: CustomPaint(
-            painter: CirclePainter(X: circleX, Y: circleY),
-          ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.yellowAccent,
         ),
-      ),
+        CustomPaint(
+          painter: CirclePainter(X: circleX, Y:circleY),
+        ),
+        CustomPaint(
+          painter: LinePainter(),
+        ),
+      ],
     );
+  }
+}
+
+class LinePainter extends CustomPainter{
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 5
+      ..strokeCap = StrokeCap.round;
+
+    Offset startingPoint = Offset(size.width/2, size.height / 2 - 10);
+    Offset endingPoint = Offset(size.width/2, size.height / 2 + 10);
+
+    canvas.drawLine(startingPoint, endingPoint, paint);
+  }
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
 
 class CirclePainter extends CustomPainter{
   double xCoordinate = 0;
   double yCoordinate = 0;
-  double circleSize = 100;
+  double circleSize = 10;
   CirclePainter({double X=0, double Y=0}){
     xCoordinate = X;
     yCoordinate = Y;
