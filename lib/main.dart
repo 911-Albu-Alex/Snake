0f8994ed7eecf8b87214c011ace64c4c014b3545
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-04 15:48:07 +0300

    added restart game button

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,15 @@ class MyHomePage extends StatefulWidget{
 
 class SnakeWidget extends State<MyHomePage>{
   late IFrameElement iFrameElement;
+  void reset(){
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration.zero,
+        pageBuilder: (_, __, ___) => MyHomePage(),
+      ),
+    );
+  }
   @override
   void initState() {
     super.initState();
@@ -52,8 +61,38 @@ class SnakeWidget extends State<MyHomePage>{
               viewType: 'iframeElement',
             ),
           ),
-      ],
-    );
-  }
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: reset,
+                  child: const Text('Restart game'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
 }
 
