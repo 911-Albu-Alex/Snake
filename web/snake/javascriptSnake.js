df747355720810d9edab24db1d495b347948eea2
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-09 20:42:07 +0300

    game now keeps track of highscore

--- web/snake/javascriptSnake.js
+++ web/snake/javascriptSnake.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     pTag = document.querySelector("div");
     newVal = document.createElement("p");
+    localStorage.setItem("highscore", String(highscore));
     newVal.innerHTML = '';
     pTag.appendChild(newVal);
 });
